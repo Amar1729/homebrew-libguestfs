08a0c8e5adab01ab968f92fd5fd27e67e50aa205
Amar1729 <amar.paul16@gmail.com>
2021-04-09 14:41:50 -0400

    automake-1.15 : fix style issues

--- automake-1.15.rb
+++ automake-1.15.rb
@@ -1,22 +1,19 @@
 class Automake115 < Formula
-  # note: can't use AT (standard for homebrew versions?) because this is a perl script,
+  # NOTE: can't use AT @ (standard for homebrew versions?) because this is a perl script,
   # so it will attempt to interpolate the '@' in the filename when used as a string
   desc "Tool for generating GNU Standards-compliant Makefiles"
   homepage "https://www.gnu.org/software/automake/"
-  #url "https://ftp.gnu.org/gnu/automake/automake-1.16.1.tar.xz"
   url "https://ftp.gnu.org/gnu/automake/automake-1.15.1.tar.xz"
-  #sha256 "5d05bb38a23fd3312b10aea93840feec685bdf4a41146e78882848165d3ae921"
   sha256 "af6ba39142220687c500f79b4aa2f181d9b24e4f8d8ec497cea4ba26c64bedaf"
-
-  conflicts_with "automake"
-
-  depends_on "autoconf"
 
   bottle do
     root_url "https://github.com/Amar1729/homebrew-formulae/releases/download/automake-1.15"
-    cellar :any_skip_relocation
-    sha256 "f81060a0b15b6846e6540bd489381eb644bf313f89000705f5ac33ce26157acf" => :mojave
+    sha256 cellar: :any_skip_relocation, mojave: "f81060a0b15b6846e6540bd489381eb644bf313f89000705f5ac33ce26157acf"
   end
+
+  depends_on "autoconf"
+
+  conflicts_with "automake"
 
   def install
     ENV["PERL"] = "/usr/bin/perl"
